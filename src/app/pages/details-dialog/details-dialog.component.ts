58288d9d5fc28f38d50114b04bc70e9dc2ae20b2
fabreduarte <fabreduarte@gmail.com>
2022-08-27 16:22:39 -0300

    Adição de caixa de Diálogo

--- src/app/pages/details-dialog/details-dialog.component.ts
+++ src/app/pages/details-dialog/details-dialog.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Exoplanet } from 'src/app/models/exoplanets';
 
 @Component({
   selector: 'app-details-dialog',
@@ -7,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailsDialogComponent implements OnInit {
 
-  constructor() { }
+  public link: string = "https://exoplanetarchive.ipac.caltech.edu/overview/";
+
+  constructor(@Inject(MAT_DIALOG_DATA) public exoplanet: Exoplanet) { 
+    this.exoplanet = exoplanet;
+  }
 
   ngOnInit(): void {
+    this.redirectLink(this.exoplanet);
+  }
+
+  redirectLink(exoplanet: Exoplanet): void {
+    let name = exoplanet.name;
+    const nameArray = name.split('');
+    for(let i =0; i < nameArray.length; i++) {
+      if(nameArray[i] === " ") {
+        nameArray[i] = "%20";
+      }
+    }
+    name = nameArray.join("");
+    this.link += name;    
   }
 
 }
